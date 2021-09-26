fa0912c3271aacdf777908b850539ce23d04ac27
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:40:46 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class even
+{
+	public static void main(String args[])
+	{
+	int i;
+	System.out.println("enter list of even number");
+	for(i=0;i<=10;i=i+1);
+	{
+	System.out.print(i);
+	}
+
+}
+
+}
