2e9258d4bc2db1ea6a518b12ecf334f72ecef291
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:10:03 +0530

    Add files via upload

---
+++ swaping.java
@@ -0,0 +1,19 @@
+class swaping
+{
+	public static void main(String args[]) 
+	{    
+       int a,b,c;
+       a=10;
+       b=40;
+       System.out.println("before swapping:");
+       System.out.println("a  =  "  + a +  " b  =  "+b);
+       c=a;
+       a=b;
+       b=c;
+
+       System.out.println("after swapping:");
+       System.out.println("a = " + a  + " b  =   "+b);
+
+
+	}
+}
