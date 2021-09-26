02223bf3a8b941d3cc590a15544db2c0b07aec8f
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:26:29 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern3
+{
+	public static void main(String args[])
+	{
+	int i,j;
+	System.out.println("pattern3: ");
+	for(i=5;i>=1;i--)
+	{
+	   for(j=5;j>=i;j--)
+       {
+         System.out.print(j);
+	    }
+	    System.out.println();
+	     
+    }
+ }
+
+}
