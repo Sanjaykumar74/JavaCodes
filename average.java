e3989fec51ba2cc1427fda001f10db9a9f124c24
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:49:45 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,20 @@
+class average
+{
+	public static void main (String args[])
+	{
+       int a,b,c,sum,avg;
+       a=10;
+       b=20;
+       c=30;
+       sum=a+b+c;
+       avg=(a+b+c)/2;
+       
+          System.out.println("value of a "+a);
+          System.out.println("value of b "+b);
+          System.out.println("value of c "+c);
+           System.out.println("sum "+sum);
+         
+          System.out.println("avg "+avg);
+          
+        }	
+	}
