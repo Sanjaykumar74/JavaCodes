5f46dde823525d91a373cddbe938b9cab2d9e4eb
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:06:51 +0530

    Add files via upload

---
+++ log.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
+class log
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the log value of a:");
+double a=sc.nextDouble();
+System.out.println("Calculate the log value:"+Math.log(a));
+}
+}
