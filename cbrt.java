7973a819156b5a88ccc38f842e5ae2269ae996a8
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:15:24 +0530

    Add files via upload

---
+++ cbrt.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
+class cbrt
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of a:");
+double a=sc.nextDouble();
+System.out.println("Calculate the cbrt value:"+Math.cbrt(a));
+}
+}
