0d82d2ac6fb8abfbe12dde63da0e59326d4e08b6
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:28:15 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern
+{
+	public static void main(String args[])
+	{
+	int i,j;
+	System.out.println("enter tables 1 to 10: ");
+	for(i=1;i<=10;i++)
+	{
+	   for(j=1;j<=10;j++)
+       {
+         System.out.print("\t" +i*j);
+	    }
+	     System.out.print("\n");
+	     
+    }
+ }
+
+}
