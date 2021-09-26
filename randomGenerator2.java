8987bc7a9afc3cf6f4afd9a9d57a5e84dd5a3db7
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:22:31 +0530

    Add files via upload

---
+++ randomGenerator2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class randomGenerator2
+{
+ public static void main(String args[])
+    {
+        Random random = new Random();
+        System.out.println("Int random account Number: "+random.nextInt(100-10)+10);
+        }
+  }  
