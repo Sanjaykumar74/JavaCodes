a5c5b1587376621b45d6e70acedafdcf7805968e
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:05:50 +0530

    Add files via upload

--- min.java
+++ min.java
@@ -1,23 +1,15 @@
-import java.util.*;
+import java.lang.Math;
+import java.util.Scanner;
 class min
 {
-	public static void main(String args[])
-	{
-	int a[]=new int[5];
-	Scanner s1=new Scanner(System.in);
-	int i;
-	System.out.println("enter 5 elements in array");
-	for(i=0;i<=4;i++)
-	{
-	 a[i]=s1.nextInt();
-	}
-	int min=a[0];
-	for(i=0;i<=4;i++)
-	{
-	 if(min>a[i])
-	  min=a[i];
-	  
-	}
-	System.out.println("min is "+min);
-	}
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of a:");
+double a=sc.nextDouble();
+System.out.println("Enter the value of b:");
+double b=sc.nextDouble();
+System.out.println("Enter the min value:");
+System.out.println("Calculate the min value:"+Math.min(a,b));
+}
 }
