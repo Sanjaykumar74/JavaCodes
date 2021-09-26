4630bece58320eafd3d8e693c34cc2024eea85a9
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:17:27 +0530

    Add files via upload

---
+++ absolute.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
+class absolute
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter any negative value:");
+int a=sc.nextInt();
+System.out.println("absolute value is="+Math.abs(a));
+}
+}
