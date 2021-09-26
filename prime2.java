d2fd954d06cc462fab9a54850c7d09190687fd29
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:02:52 +0530

    Add files via upload

--- prime2.java
+++ prime2.java
@@ -1,20 +1,26 @@
 import java.util.*;
 class prime2
 {
-	public static void main(String args[])
-	{
-	int i,j;
-	System.out.println("enter prime number 1 to 100: ");
-	for(i=1;i<=100;i++)
-	{
-	   for(j=2;j<i;j++)
-       {
-       	if(i%j==0)
-         break;  
-	  }
-	  if(i==j)
-	  System.out.print("\t" +i);
- }
-
-  }
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number:");
+int no=sc.nextInt();
+int temp=0;
+for(int i=2;i<=no-1;i++)
+{
+if(no%i==0)
+{
+temp=temp+1;
+}
+}
+if(temp>0)
+{
+System.out.println("Number is not prime:");
+}
+else
+{
+System.out.println("Number is  prime:");
+}
+}
 }
