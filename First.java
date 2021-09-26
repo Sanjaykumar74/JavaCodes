ed0224a3e8fc3f772081a3e320624cab6f47bf5e
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:38:43 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,80 @@
+import java.util.*;
+class First
+{
+	public static void main(String args[])
+	{
+		//int,float,char,String,boolean,long
+	/*int marks=29;
+	float averagemarks=28.454f;
+	char grad='A';
+	String name="Sanjay Kumar";
+	boolean isvalid = false;  
+	long bignumber  = 2546767288877l;
+	System.out.println(averagemarks);
+	System.out.println(name);
+	System.out.println(grad);
+    System.out.println(marks);
+    System.out.println(isvalid);
+    System.out.println(bignumber);
+    int firstNumber=1234;
+    int secondNumber=234;
+
+
+    //int sum= firstNumber+secondNumber;
+    double sum=(double)firstNumber%(double)secondNumber;
+    System.out.println(sum);
+    int a=12;
+    a++;
+    ++a;
+    a--;
+    System.out.println(a);*/
+
+  
+   // double age;
+    //String name;
+    //System.out.println("Enter your name and age ");
+    //name=sc.nextLine();
+     //age =sc.nextDouble();
+   //System.out.println("name "+name);
+    //System.out.println("age "+2*age);
+//    int age=18;
+  //  if (age>18)
+   
+    //{
+     //   System.out.println("you can vote");
+   // }    
+  //  else
+    //{
+      //  System.out.println("you can not vote");
+    //}
+    Scanner sc =new Scanner(System.in);
+    int a,b,c;
+    System.out.println("enter three numbers");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+
+      if (a>b)  
+       {
+        if(a>c)
+        System.out.println("greatest of a");
+      else
+        System.out.println("greatest of c");
+        }
+     else
+     {
+      if(b>c)
+     System.out.println("greatest of b");
+    else
+      System.out.println("greatest of c");
+    }
+    }
+}
+      
+
+
+
+
+  
+
+
