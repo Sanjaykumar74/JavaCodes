aaa2c8b4d31f44a0e4a64f290f4e5ca2002304a4
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:29:55 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class num
+{
+	public static void main(String[]args)
+
+
+	{
+		byte b1,b2,c;
+		b1=20;
+	    b2=20;
+		c=(byte)(b1+b2);
+		System.out.println("c "+c);
+	}
+}
+
