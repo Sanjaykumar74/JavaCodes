62b6c4aec596282592a91c8b2b84d91e7c4f6f01
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:43:02 +0530

    Add files via upload

---
+++ character.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class character
+
+{
+	public static void main(String args[])
+	{
+	char chr;
+	Scanner s1=new Scanner(System.in);
+	System.out.println("enter alphabet");
+     chr=s1.nextLine().charAt(0);
+     if(chr=='a')
+     {
+     	System.out.println("alphabet is vowal "+chr);
+     }
+     else if(chr=='e')
+     {
+     	System.out.println("alphabet is vowal "+chr);
+     }
+     else if(chr=='i')
+     { 
+     	System.out.println("alphabet is vowal "+chr);
+     }
+     else if(chr=='o')
+     {
+     	System.out.println("alphabet is vowal "+chr);
+     }
+     else if(chr=='u')
+     {
+     	System.out.println("alphabet is vowal "+chr);
+     }
+     else
+     	System.out.println("alphabet is not vowal "+chr);
+
+     }//end of main
+}//end of class
+	
