ce0f3e8fe229ebd494f612a911457f6e8649cb52
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:08:53 +0530

    Add files via upload

---
+++ team.java
@@ -0,0 +1,15 @@
+class team
+{
+	public static void main (String args[])
+	{
+		double pi,r,area,rf;
+		pi =3.14;
+		r=5;
+		area=(pi*r*r);
+		rf=(2*pi*r);
+		System.out.println("value of radius="+r);
+		System.out.println("area of circle="+area);
+	    System.out.println("Radius of circle ="+rf);
+
+	}
+}
