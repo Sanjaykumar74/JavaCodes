21f10259a9436ef0d340a47ab0d971a096bf9cb1
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:12:03 +0530

    Add files via upload

--- factorial.java
+++ factorial.java
@@ -1,17 +1,16 @@
 import java.util.*;
 class factorial
 {
-	public static void main(String args[])
-	{
-	 Scanner s1=new Scanner(System.in);
-	 int i,n,fact=1;
-	 System.out.println("enter number");
-	 n=s1.nextInt();
-	 for(i=1;i<=n;i++)
-	 {
-	 	fact=fact*i;
-	 }
-	 System.out.println("factorial "+fact);
-	}
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the factorial number:");
+int n=sc.nextInt();
+int factorial=1;
+for(int i=1;i<=n;i++)
+{
+factorial=factorial*i;
 }
-
+System.out.println("factorial:"+factorial);
+}
+}
