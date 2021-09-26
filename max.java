f5c784f44ca61896efd4c02529e45dbb0e962df0
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:06:21 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+import java.util.Scanner;
+class max
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of a:");
+double a=sc.nextDouble();
+System.out.println("Enter the value of b:");
+double b=sc.nextDouble();
+System.out.println("Calculate the max value:"+Math.max(a,b));
+}
+}
