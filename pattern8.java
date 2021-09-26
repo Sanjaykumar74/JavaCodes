7b57852d6749b7bdb745cd15608e6052a3d4dac4
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:24:57 +0530

    Add files via upload

---
+++ pattern8.java
@@ -0,0 +1,23 @@
+class pattern8
+{
+	public static void main(String args[])
+	{
+	int i,j,k;
+	System.out.println("pattern8 :")
+
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			System.out.print(" ");
+		  }
+	       for(k=5;k>=i;k--)
+           {
+           System.out.print(k);
+	     }
+	       System.out.println();
+	     
+        }
+ }
+
+}
