f8986c74d9ad172a17227d1b1b256219f1e5e0a3
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:01:42 +0530

    Add files via upload

---
+++ vowal.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class vowal
+{
+	public static void main(String args[])
+	{
+	Scanner s1=new Scanner(System.in);
+	char chr;
+	System.out.println("enter alphabet a,e,i,o,u ");
+	 chr=s1.nextLine().charAt(0);
+	 switch(chr)
+	 {
+	 	case 'a':System.out.println("alphbet is vowal "+chr);
+	 	break;
+	 	case 'e':System.out.println("alphbet is vowal "+chr);
+	 	break;
+	 	case 'i':System.out.println("alphbet is vowal "+chr);
+	 	break;
+	 	case 'o':System.out.println("alphbet is vowal "+chr);
+	 	break;
+	 	case 'u':System.out.println("alphbet is vowal "+chr);
+	 	break;
+	 	default:System.out.println("alphbet is not vowal "+chr);
+
+	 }
+	 
+
+
+
+}
+}
