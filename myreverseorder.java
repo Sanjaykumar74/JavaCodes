2de498b0e72fc9ad257c402c2b12daf8266984a4
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:30:28 +0530

    Add files via upload

---
+++ myreverseorder.java
@@ -0,0 +1,28 @@
+import java .util.*;
+class myreverseorder
+{
+	public static void main(String args[])
+	{
+	int a[]=new int [5];
+	int i;
+	Scanner s1=new Scanner(System.in);
+	System.out.println("enter any 5 elements in the array:");
+	for(i=0;i<5;i++)
+	{
+       a[i]=s1.nextInt();
+
+	} 
+	System.out.println("elements in the array : \n index \tvalue");
+	for(i=0;i<5;i++)
+	{
+		System.out.println(i+ "\t" +a[i]);
+		
+       }
+       System.out.println("elements in the array are reverse order : \n index \tvalue");
+       for(i=4;i>=0;i--)
+       {
+       	System.out.println(i+  "\t" +a[i]);
+       }
+	}
+
+}
