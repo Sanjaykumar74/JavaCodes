6f245dd56e56ff58b832706f4802df0105633133
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:50:54 +0530

    Add files via upload

---
+++ abcd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class abcd
+{
+	public static void main(String args[])
+	{
+		Scanner s1=new Scanner(System.in);
+		int n;                                                                                                                                                                        
+		System.out.println("enter number ");
+		n=s1.nextInt();
+		if (n%2==0)
+		{
+			System.out.println("nunber is even");
+		}
+		else
+			System.out.println("number is odd ");
+	}
+}
