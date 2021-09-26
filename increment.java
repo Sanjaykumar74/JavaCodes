e092b548f26dc5d097f0591aa88cab38f156585d
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:36:44 +0530

    Add files via upload

---
+++ increment.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class increment
+{
+	public static void main(String args[])
+	{
+	Scanner s1=new Scanner(System.in);
+	int a=115;
+
+	System.out.println(++a);
+	System.out.println(++a);
+	System.out.println(a++);
+	System.out.println(a--);
+	System.out.println(--a);
+	System.out.println(a++);
+	System.out.println(--a);
+
+}
+
+}
