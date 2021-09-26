d62235c47f34bc0185dff859dfec2dcf84fe98e7
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:31:03 +0530

    Add files via upload

---
+++ mybreak.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class mybreak
+{
+	public static void main(String args[])
+	{
+	int i;
+	for(i=1;i<=5;i++)
+	{
+	   if(i==3)
+	   break;
+	   
+	   else
+	   	System.out.println(i);
+	 }
+	}
+}
