431edf0fe28770698dc5a02e44e0f293b7c0a35d
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:11:42 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,17 @@
+class swap
+{
+	public static void main(String args[])
+	{
+	 int a,b,c;
+	 a=10;
+	 b=15;
+	 System.out.println("Before swapping:");
+	 System.out.println("a = "+a+" b = "+b);
+	 c=a;
+	 a=b;
+	 b=c;
+	 System.out.println("After swapping:");
+	 System.out.println("a = "+a+" b = "+b);
+}
+
+}
