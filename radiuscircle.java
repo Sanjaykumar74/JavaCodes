f19d351d21405bb0f9ba86ed020ffac606961a77
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:02:23 +0530

    Add files via upload

---
+++ radiuscircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class radiuscircle
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Diameter of circle:");
+float Diameter=sc.nextFloat();
+float radius=Diameter/2;
+System.out.println("radius="+radius);
+}
+}
