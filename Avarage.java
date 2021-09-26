49875ee46f19945a3af91359695d0b5bd001fc65
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:50:08 +0530

    Add files via upload

---
+++ Avarage.java
@@ -0,0 +1,15 @@
+class Avarage
+{
+	public static void main(String arg[])
+	{
+	int a,b,c,avg;
+	 a=10;
+	 b=20;
+	 c=30;
+	avg=(a+b+c)/3;
+	System.out.println("value of a=  "+a);
+	System.out.println("value of b=  "+b);
+	System.out.println("value of c  "+c);
+	System.out.println("avg "+avg);
+	}
+ 
