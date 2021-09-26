6f58ee60290f91ba3f29d10a294255678fb99bb2
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:29:16 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class odd
+{
+	public static void main(String args[])
+	{
+	int i;
+	System.out.println("enter lists of odd number");
+	for(i=1;i<=10;i=i+2);
+	{
+	System.out.println(i);
+	}
+
+
+
+	}
+
+}
