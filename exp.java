fb2e57597159cd38b72b0fc915ca2c0025e2b145
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:12:47 +0530

    Add files via upload

---
+++ exp.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+import java.util.Scanner;
+class exp
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the exponent value of a:");
+double a=sc.nextDouble();
+System.out.println("Calculate the exp value:"+Math.exp(a));
+}
+}
