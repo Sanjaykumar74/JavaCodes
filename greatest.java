05e77aa3357d67284fb3f08f2a970fb0ae143077
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:38:07 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class greatest
+{
+	public static void main(String args[])
+	{
+	Scanner s1=new Scanner(System.in);
+	int a,b;
+	System.out.println("enter 2 number a and b ");
+	a=s1.nextInt();
+	b=s1.nextInt();
+   if(a>b)
+    {
+  	   System.out.println("greatest of a ");
+	}
+	
+	else
+	{
+	   System.out.println("greatest of b ");
+	}
+
+}
+
+}
+
+
