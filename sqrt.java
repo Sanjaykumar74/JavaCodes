bb14693526c92467698e20d5c68997215e014a8f
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:59:36 +0530

    Add files via upload

---
+++ sqrt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sqrt
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of a:");
+int a=sc.nextInt();
+System.out.println("Calculate the sqrt value:"+Math.sqrt(a));
+}
+}
