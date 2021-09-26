915ca169e76e34438c37307ef31cb74c3340ed50
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 20:04:08 +0530

    Add files via upload

---
+++ number2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class number2
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of a:");
+double a=sc.nextDouble();
+System.out.println("Enter the value of b:");
+double b=sc.nextDouble();
+System.out.println("Enter the value of c:");
+double c=sc.nextDouble();
+double d=(a+b)*(c/b);
+System.out.println("Calculate the d number:"+d);
+}
+}
+
