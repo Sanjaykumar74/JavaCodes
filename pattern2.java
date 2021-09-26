d7525ef62bb15ff0b1ae2a15e5073888ce7d7b90
Sanjay kumar <84563759+Sanjaykumar74@users.noreply.github.com>
2021-09-26 19:26:52 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern2
+{
+	public static void main(String args[])
+	{
+	int i,j;
+	System.out.println("pattern2: ");
+	for(i=1;i<=5;i++)
+	{
+	   for(j=1;j<=i;j++)
+       {
+         System.out.print(j);
+	    }
+	    System.out.println();
+	     
+    }
+ }
+
+}
